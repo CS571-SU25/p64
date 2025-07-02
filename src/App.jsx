550bb4f2ad0875647243614e623e6dc3064778a1
morgan <mmnelson7@wisc.edu>
2025-07-01 19:11:13 -0500

    made nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import "./App.css";
 import { HashRouter, Routes, Route } from "react-router";
-import AboutUs from "./components/CoachingOpportunities";
-import OtherInfo from "./components/BookLesson";
 import Home from "./components/Home";
 import CoachingOpportunities from "./components/CoachingOpportunities";
 import BookLesson from "./components/BookLesson";
 import FavoriteProducts from "./components/FavoriteProducts";
 import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route
-          path="/coaching-opportunities"
-          element={<CoachingOpportunities />}
-        ></Route>
-        <Route path="/book-lesson" element={<BookLesson />}></Route>
-        <Route path="/favorite-products" element={<FavoriteProducts />}></Route>
-        <Route path="/about-me" element={<AboutMe />}></Route>
-        <Route path="/contact-me" element={<ContactMe />}></Route>
-      </Routes>
-    </HashRouter>
+    <div>
+      <NavBar />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/coaching-opportunities"
+            element={<CoachingOpportunities />}
+          ></Route>
+          <Route path="/book-lesson" element={<BookLesson />}></Route>
+          <Route
+            path="/favorite-products"
+            element={<FavoriteProducts />}
+          ></Route>
+          <Route path="/about-me" element={<AboutMe />}></Route>
+          <Route path="/contact-me" element={<ContactMe />}></Route>
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavBar() {
+  return (
+    <Navbar
+      bg="dark"
+      data-bs-theme="dark"
+      style={{ position: "absolute", top: 0, left: 0, width: "100%" }}
+    >
+      <Container>
+        <Navbar.Brand href="/">Chapman Pickleball</Navbar.Brand>
+        <Nav>
+          <Nav.Link href="#coaching-opportunities">
+            Coaching Opportunities
+          </Nav.Link>
+          <Nav.Link href="#book-lesson">Book a Lesson</Nav.Link>
+          <Nav.Link href="#favorite-products">Favorite Products</Nav.Link>
+          <Nav.Link href="#about-me">About Me</Nav.Link>
+          <Nav.Link href="#contact-me">Contact Me</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
