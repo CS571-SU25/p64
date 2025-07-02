e8288b0b074bd70e29dbd7d6d401376dd9762a42
morgan <mmnelson7@wisc.edu>
2025-07-01 21:10:20 -0500

    creating a form based on selected lesson

--- src/components/BookLesson.jsx
+++ src/components/BookLesson.jsx
@@ -1,3 +1,75 @@
+import IndividualLessonForm from "./IndividualLessonForm";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { useState, useEffect } from "react";
+
 export default function BookLesson() {
-  return <h1>Lesson Types</h1>;
+  const [lesson, setLesson] = useState();
+
+  const lessonTypes = [
+    {
+      id: 1,
+      title: "Individual Private Lesson",
+      price: "$65/hr",
+      description:
+        "Work directly with Colin in a personalized, one-on-one coaching environment designed to elevate every aspect of your game. Whether you're just getting started or looking to refine advanced techniques, Colin provides tailored instruction, immediate feedback, and proven strategies to help you reach your pickleball potential.",
+    },
+    {
+      id: 2,
+      title: "Online Film Review",
+      price: "$50 per 30 minutes of footage",
+      description:
+        "Submit up to 30 minutes of game footage and identify which player you are. Colin will personally review your match, analyzing key moments and providing a detailed video breakdown with insights, strategy adjustments, and specific areas for improvement. It’s like having a private coaching session on your own schedule.",
+    },
+    {
+      id: 3,
+      title: "Three and Me",
+      price: "$90/hr- 30 per person",
+      description:
+        "You and up to two others can book a private coaching session with Colin that blends personalized instruction with live gameplay. These small-group sessions allow for focused skill development, strategic guidance, and real-time feedback, all while playing in a competitive, game-like environment.",
+    },
+    {
+      id: 4,
+      title: "Online Practice Guide",
+      price: "$40",
+      description:
+        "This guide is a curated roadmap designed to help you reach the 4.0 level on your own schedule. Packed with targeted practice drills and intentional progressions, it empowers you to train effectively and independently. Whether you're working on fundamentals, refining strategy, or building consistency, this guide will elevate your training.",
+    },
+  ];
+
+  function handeLessonSelection(value) {
+    console.log(value);
+    setLesson(() => {
+      const lesson = lessonTypes.find((l) => l.id === parseInt(value));
+
+      return lesson;
+    });
+  }
+
+  return (
+    <div>
+      <Form.Group controlId="lesson-type">
+        <Form.Label>Lesson Type</Form.Label>
+        <Form.Select onChange={(e) => handeLessonSelection(e.target.value)}>
+          {lessonTypes.map((lesson) => {
+            return (
+              <option key={lesson.id} value={lesson.id}>
+                {lesson.title}
+              </option>
+            );
+          })}
+        </Form.Select>
+      </Form.Group>
+
+      <Container>
+        {!lesson ? (
+          <div></div>
+        ) : (
+          <IndividualLessonForm key={lesson.id} {...lesson} />
+        )}
+      </Container>
+    </div>
+  );
 }
