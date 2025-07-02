03d70daf70d42ac76a2ef13689992770f67f6e44
morgan <mmnelson7@wisc.edu>
2025-07-01 20:08:04 -0500

    started contact form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,3 +1,31 @@
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
 export default function ContactMe() {
-  return <h1>Contact me</h1>;
+  return (
+    <Form style={{ width: "100%" }}>
+      <Form.Group as={Row} controlId="email">
+        <Form.Label column>Email</Form.Label>
+        <Col>
+          <Form.Control placeholder="name@email.com" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="description">
+        <Form.Label column>Description</Form.Label>
+
+        <Col>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Please write your question here"
+          />
+        </Col>
+      </Form.Group>
+
+      <Button variant="primary">Submit</Button>
+    </Form>
+  );
 }
