10e02ee55f0c2e4223e9833294467e25eda5cbdf
morgan <mmnelson7@wisc.edu>
2025-07-06 09:32:46 -0500

    updated bottom pane

--- src/components/CoachingOpportunities.jsx
+++ src/components/CoachingOpportunities.jsx
@@ -1,10 +1,17 @@
 import LessonTypesGrid from "./LessonTypesGrid";
+import Stack from "react-bootstrap/Stack";
 
 export default function CoachingOpportunities() {
   return (
-    <div style={{ margin: "6rem", width: "100%" }}>
+    <div style={{ marginTop: "6rem" }}>
       <h2>Coaching Opportunities</h2>
-      <LessonTypesGrid showLongDescription={true} height={"600px"} />
+      <Stack
+        style={{ backgroundColor: "white", textAlign: "left", width: "100%" }}
+        fluid
+      >
+        <h3>In Person Lessons</h3>
+        <LessonTypesGrid showLongDescription={true} height={"600px"} />
+      </Stack>
     </div>
   );
 }
