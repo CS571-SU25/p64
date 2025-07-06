d0141ab9468c7043938e99448fafddebce8bd00b
morgan <mmnelson7@wisc.edu>
2025-07-05 21:48:12 -0500

    moved out view product button

--- src/components/FavoriteProductCard.jsx
+++ src/components/FavoriteProductCard.jsx
@@ -3,17 +3,22 @@ import Card from "react-bootstrap/Card";
 
 export default function FavoriteProductCard(props) {
   return (
-    <Card style={{ width: "18rem", height: "500px", margin: "auto" }}>
+    <Card
+      style={{
+        width: "18rem",
+        height: "500px",
+        textAlign: "center",
+      }}
+    >
       <Card.Title>{props.favoriteTitle}</Card.Title>
       <Card.Subtitle>{props.name}</Card.Subtitle>
       <Card.Body>
         <Card.Img variant="top" src={props.img} alt={props.name} />
-
         <Card.Text>{props.description}</Card.Text>
-        <Button variant="primary" target="_blank" href={props.externalLink}>
-          View Product
-        </Button>
       </Card.Body>
+      <Button variant="primary" target="_blank" href={props.externalLink}>
+        View Product
+      </Button>
     </Card>
   );
 }
