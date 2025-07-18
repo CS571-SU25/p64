39ee79f080a437d1d3ff911a288bae554c048c83
morgan <mmnelson7@wisc.edu>
2025-07-17 19:45:42 -0500

    reviewed pickleball journey section

--- src/components/About/CardTextBanner.jsx
+++ src/components/About/CardTextBanner.jsx
@@ -3,20 +3,21 @@ import { Image, Col, Row } from "react-bootstrap";
 export default function CardTextBanner(props) {
   return (
     <Row style={{ margin: "2rem" }}>
-      <Col sm={12} md={12} lg={4}>
+      <Col sm={12} md={12} lg={4} xl={5}>
         <Image style={{ width: "100%" }} src={props.src} alt={props.alt} />
       </Col>
       <Col
         sm={12}
         md={12}
         lg={8}
+        xl={6}
         style={{
-          fontSize: "1.25rem",
           lineHeight: "2rem",
+          fontSize: "1.1rem",
           textAlign: "left",
         }}
       >
-        <h3>{props.heading}</h3>
+        <h3 style={{ margin: "1rem 0" }}>{props.heading}</h3>
         <h4>{props.year}</h4>
         <div>{props.text}</div>
       </Col>
--- src/components/About/GettingIntoPickleball.jsx
+++ src/components/About/GettingIntoPickleball.jsx
@@ -1,12 +1,13 @@
 import CardTextBanner from "./CardTextBanner";
 
+// TODO: need new images for these
 const journeyData = [
   {
     src: "./about-colin.jpg",
     alt: "Colin spiking a pickleball",
     heading: "UW-Eau Claire Pickleball Club",
     year: "2024",
-    text: "Colin was first introduced to pickleball while he was completing his undergraduate degree at the University of Wisconsin-Eau Claire. At the university, he met friends who were members of the University of Wisconsin-Eau Claire Club Pickleball Team. After playing pickleball a few times with friends, Colin joined the club team. He had a great time attending the club practices, and since he grew up playing tennis, he developed strong pickleball skills very quickly. The next academic year after joining, Colin become a Co-President of the club. In this role, he led club practices and organized the team's tournament travel. He led the team to attending the Club Picklball National tournament for the very first time! This experience made Colin find pickleball really enjoyable and fun. He loved being apart of a team and leading an organization that he was passionate about. Ever since this experience, Colin has loved pickleball.",
+    text: "Colin was first introduced to pickleball while he was completing his undergraduate degree at the University of Wisconsin-Eau Claire. At the university, he met friends who were members of the school's Club Pickleball Team. Colin later joined the club team. He had a great time attending the club practices, and since he grew up playing tennis, he developed strong pickleball skills very quickly. The next academic year after joining, Colin become a Co-President of the club. In this role, he led club practices and organized the team's tournament travel.",
   },
   {
     src: "./about-colin.jpg",
