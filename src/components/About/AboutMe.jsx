4bd747d7e7fd5a1af624a8788a81fbc86ffe2588
morgan <mmnelson7@wisc.edu>
2025-07-17 19:33:48 -0500

    cleaning up about me section

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -4,7 +4,6 @@ import GettingIntoPickleball from "./GettingIntoPickleball";
 import TennisBackground from "./TennisBackground";
 
 export default function AboutMe() {
-  /* Text from: https://www.lipsum.com/feed/html */
   return (
     <div>
       <AboutColin />
--- src/components/homePage/AboutColin.jsx
+++ src/components/homePage/AboutColin.jsx
@@ -27,7 +27,7 @@ export default function AboutColin() {
         Qualifiers, bringing high-level experience and competitive insight to
         his coaching. Before making the switch to pickleball, he played three
         years of NCAA Division III tennis at the University of Wisconsin–Eau
-        Claire, where he developed a strong foundation in court strategy and
+        Claire where he developed a strong foundation in court strategy and
         athletic performance. Now based in Verona, Wisconsin, Colin is
         passionate about helping players elevate their game through personalized
         instruction, video analysis, and game-based training.
