4eb2aff86acc1431cc66e780a4bec109fb98d98c
morgan <mmnelson7@wisc.edu>
2025-07-19 11:48:34 -0500

    updated in person card

--- src/components/bookedLessons/BookedLessonCard.jsx
+++ src/components/bookedLessons/BookedLessonCard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 export default function BookedLessonCard(props) {
   const [lessonName, setLessonName] = useState(null);
+  const [lessonLocation, setLessonLocation] = useState(null);
 
   const getName = () => {
     if (props.participants.length > 1) {
@@ -12,31 +13,46 @@ export default function BookedLessonCard(props) {
     return "Individual Private Lesson";
   };
 
+  const getLocation = () => {
+    if (props.location === "1") {
+      return "Garner Park";
+    }
+
+    return "Pickleball Pro Courts";
+  };
+
   useEffect(() => {
     setLessonName(getName);
+    setLessonLocation(getLocation);
   }, []);
 
   return (
-    <Card style={{ height: "250px", margin: "auto" }}>
-      <Card.Body>
-        <Row>
-          <Col>
-            <Card.Title>{lessonName}</Card.Title>
-            <Card.Subtitle>{props.date}</Card.Subtitle>
-            <div>{props.time}</div>
-            <Card.Text>Fake Location Here</Card.Text>
-          </Col>
-          <Col style={{ margin: "auto" }}>
-            <h6>Participants</h6>
-            <ListGroup className="list-group-flush">
-              {props.participants.map((p, index) => {
-                return <ListGroup.Item key={index}>{p}</ListGroup.Item>;
-              })}
-            </ListGroup>
-          </Col>
-        </Row>
+    <Card style={{ margin: "1rem auto" }}>
+      <Card.Title>{lessonName}</Card.Title>
+      <Card.Subtitle>
+        {props.date} at {props.time}
+        <br></br>
+        {lessonLocation}
+      </Card.Subtitle>
+      <hr />
+      <Card.Body style={{ padding: 0 }}>
+        <div style={{ height: "150px", margin: "auto" }}>
+          {props.participants.length == 1 ? (
+            <h5>Participant</h5>
+          ) : (
+            <h5>Participants</h5>
+          )}
+          <ListGroup className="list-group-flush">
+            {props.participants.map((p, index) => {
+              return <ListGroup.Item key={index}>{p}</ListGroup.Item>;
+            })}
+          </ListGroup>
+        </div>
+        <hr />
+        <Card.Text>
+          <em>Booked by {props.name}</em>
+        </Card.Text>
       </Card.Body>
-      <em>Booked by {props.name}</em>
     </Card>
   );
 }
