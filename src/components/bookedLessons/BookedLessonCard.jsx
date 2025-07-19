ea74b5b284e71777bbe060cc6ffc6c3caa64cc26
morgan <mmnelson7@wisc.edu>
2025-07-18 20:53:11 -0500

    working on making lessons viewable

---
+++ src/components/bookedLessons/BookedLessonCard.jsx
@@ -0,0 +1,40 @@
+import { Card, ListGroup, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+export default function BookedLessonCard(props) {
+  const [lessonName, setLessonName] = useState(null);
+
+  const getName = () => {
+    if (props.participants.length > 1) {
+      return "Three and Me";
+    }
+
+    return "Individual Private Lesson";
+  };
+
+  useEffect(() => {
+    setLessonName(getName);
+  }, []);
+
+  return (
+    <Card style={{ height: "250px", margin: "auto" }}>
+      <Card.Body>
+        <Row>
+          <Col>
+            <Card.Title>{lessonName}</Card.Title>
+            <Card.Subtitle>{props.date}</Card.Subtitle>
+            <Card.Text>Fake Location Here</Card.Text>
+          </Col>
+          <Col style={{ margin: "auto" }}>
+            <h6>Participants</h6>
+            <ListGroup className="list-group-flush">
+              {props.participants.map((p, index) => {
+                return <ListGroup.Item key={index}>{p}</ListGroup.Item>;
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/components/bookedLessons/BookedLessonsPages.jsx
+++ src/components/bookedLessons/BookedLessonsPages.jsx
@@ -1,3 +1,56 @@
+import { useEffect, useState } from "react";
+import { Container, Col, Row, Form } from "react-bootstrap";
+import BookedLessonCard from "./BookedLessonCard";
+
 export default function BookedLessonsPage(props) {
-  return <div>here</div>;
+  const [bookedLessons, setBookedLessons] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch(`https://cs571api.cs.wisc.edu/rest/su25/bucket/lessons`, {
+      headers: {
+        "X-CS571-ID": CS571.getBadgerId(),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json.results);
+        const sortedLessonsByDate = Object.values(json.results)
+          .filter((result) => result.location !== null)
+          .sort((first, next) => new Date(first.date) - new Date(next.date));
+        sortedLessonsByDate.forEach((l) => {
+          const formattedDate = new Date(l.date);
+          l.date = formattedDate.toLocaleString("en-US", {
+            dateStyle: "medium",
+            timeStyle: "short",
+          });
+        });
+        setBookedLessons(sortedLessonsByDate);
+      });
+  }, []);
+
+  return (
+    <Container style={{ margin: "2rem auto" }}>
+      <Form.Group>
+        <Form.Label required htmlFor="name">
+          Participant Name
+        </Form.Label>
+        <Form.Control required id="name" placeholder="Search by name" />
+        <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+      </Form.Group>
+      {bookedLessons.length == 0 ? (
+        <div>There are no lessons</div>
+      ) : (
+        <Row>
+          {bookedLessons.map((lesson) => {
+            return (
+              <Col key={lesson.date} sm={12} md={6} lg={4}>
+                <BookedLessonCard {...lesson} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
+    </Container>
+  );
 }
