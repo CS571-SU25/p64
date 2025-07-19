96ef5218662f119aa5f1b177b945eb6dd2d242a4
morgan <mmnelson7@wisc.edu>
2025-07-19 11:59:32 -0500

    added search options

--- src/components/bookedLessons/PendingFilmReviewGrid.jsx
+++ src/components/bookedLessons/PendingFilmReviewGrid.jsx
@@ -1,5 +1,8 @@
-import { Card, ListGroup, Row, Col, Container } from "react-bootstrap";
+import { Card, Row, Col, Container, Form } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function PendingFilmReviewGrid(props) {
   const [pendingReviews, setPendingReviews] = useState([]);
@@ -29,7 +32,55 @@ export default function PendingFilmReviewGrid(props) {
   }, [props.allLessons]);
 
   return (
-    <Container style={{ minHeight: "80vh" }}>
+    <Container style={{ margin: "2rem auto" }}>
+      <Row style={{ textAlign: "left", margin: "1rem auto" }}>
+        <Col sm={12} md={6} style={{ margin: "1rem 0" }}>
+          <Form.Group>
+            <Form.Label required htmlFor="participantName">
+              Participant Name
+            </Form.Label>
+            <Form.Control
+              // value={participantName}
+              // onChange={(e) => setParticipantName(e.target.value)}
+              id="participantName"
+              placeholder="Search by participant name"
+            />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6} style={{ margin: "1rem 0" }}>
+          <Form.Group>
+            <Form.Label required htmlFor="bookedByName">
+              Booked by Name
+            </Form.Label>
+            <Form.Control
+              // value={bookedByName}
+              // onChange={(e) => setBookedByName(e.target.value)}
+              id="bookedByName"
+              placeholder="Search by the person who booked the lesson"
+            />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6} style={{ margin: "1rem 0" }}>
+          <Row>
+            <Form.Group>
+              <Col sm={12}>
+                <Form.Label htmlFor="submittedDate">Submitted Date</Form.Label>
+              </Col>
+              <Col sm={12}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    id="submittedDate"
+                    label="Search by submitted date"
+                    // value={dateValue}
+                    // onChange={(newValue) => setDateValue(newValue)}
+                  />
+                </LocalizationProvider>
+              </Col>
+            </Form.Group>
+          </Row>
+        </Col>
+      </Row>
+      <hr />
       {pendingReviews.length == 0 ? (
         <div>There are no film reviews currently pending.</div>
       ) : (
@@ -39,7 +90,9 @@ export default function PendingFilmReviewGrid(props) {
               <Col key={index} sm={12} md={6} lg={4}>
                 <Card style={{ margin: "1rem auto" }}>
                   <Card.Title>Pending Review</Card.Title>
-                  <Card.Subtitle>{review.participants[0]}</Card.Subtitle>
+                  <Card.Subtitle>
+                    Participant: {review.participants[0]}
+                  </Card.Subtitle>
                   <hr />
                   <Card.Body style={{ padding: 0, margin: 0 }}>
                     <em>
