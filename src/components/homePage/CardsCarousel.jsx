f5fda80f3c0f94adcfa49df7b35b3147f796695b
morgan <mmnelson7@wisc.edu>
2025-07-28 19:57:39 -0500

    added contrast for arrows

--- src/components/homePage/CardsCarousel.jsx
+++ src/components/homePage/CardsCarousel.jsx
@@ -2,14 +2,29 @@ import FavoriteProductCard from "../favoriteProducts/FavoriteProductCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./CardsCarousel.css";
 
 export default function CoachingCardsCarousel(props) {
+  const PreviousArrow = ({ onClick }) => (
+    <div onClick={onClick} className="slider-arrow previous">
+      ◀
+    </div>
+  );
+
+  const NextArrow = ({ onClick }) => (
+    <div onClick={onClick} className="slider-arrow next">
+      ▶
+    </div>
+  );
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PreviousArrow />,
     responsive: [
       {
         breakpoint: 1300,
