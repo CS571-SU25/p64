8ef1abfd09b6054b23b223c5950f9b21a18c85f5
morgan <mmnelson7@wisc.edu>
2025-08-03 10:25:02 -0500

    typo proof read

--- src/components/homePage/AboutColin.jsx
+++ src/components/homePage/AboutColin.jsx
@@ -22,7 +22,7 @@ export default function AboutColin() {
         }}
       >
         Colin is a 5.0+ pickleball player who actively competes in PPA Pro
-        Qualifiers, bringing high-level experience and competitive insight to
+        Qualifiers, bringing high-level experience and competitive insights to
         his coaching. Before making the switch to pickleball, he played three
         years of NCAA Division III tennis at the University of Wisconsin–Eau
         Claire where he developed a strong foundation in court strategy and
