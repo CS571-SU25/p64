1f49a394b2c563c5f7b81f83bd440f0bc278482a
morgan <mmnelson7@wisc.edu>
2025-08-04 18:29:10 -0500

    updated html for of participant form

--- src/components/bookLesson/ParticipantsFormGroup.jsx
+++ src/components/bookLesson/ParticipantsFormGroup.jsx
@@ -4,20 +4,20 @@ export default function ParticipantsFormGroup(props) {
   return (
     <>
       <Form.Group style={{ margin: "1rem 0" }}>
-        <Form.Label htmlFor="Participant 1">Participant 1</Form.Label>
+        <Form.Label htmlFor="participant1">Participant 1</Form.Label>
         <Form.Control
           ref={props.participant1}
-          id="Participant 1"
+          id="participant1"
           required
           type="name"
           placeholder="Enter name"
         />
       </Form.Group>
       <Form.Group style={{ margin: "1rem 0" }}>
-        <Form.Label htmlFor="Participant 2">Participant 2</Form.Label>
+        <Form.Label htmlFor="participant2">Participant 2</Form.Label>
         <Form.Control
           ref={props.participant2}
-          id="Participant 2"
+          id="participant2"
           required
           type="name"
           placeholder="Enter name"
@@ -27,9 +27,9 @@ export default function ParticipantsFormGroup(props) {
         </Form.Control.Feedback>
       </Form.Group>
       <Form.Group style={{ margin: "1rem 0" }}>
-        <Form.Label htmlFor="Participant 3">Participant 3</Form.Label>
+        <Form.Label htmlFor="participant3">Participant 3</Form.Label>
         <Form.Control
-          id="Participant 3"
+          id="participant3"
           type="name"
           placeholder="Enter name"
           ref={props.participant3}
