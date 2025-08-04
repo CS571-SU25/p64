090e297629def3bfa63f3871a6a26fabab356434
morgan <mmnelson7@wisc.edu>
2025-08-03 20:15:24 -0500

    updated alt

--- src/consts/pickleballJourneyData.js
+++ src/consts/pickleballJourneyData.js
@@ -3,7 +3,7 @@ export const pickleballJourneyData = [
   {
     id: 1,
     src: "./club-group-pic.JPG",
-    alt: "Club Pickleball Group Picture",
+    alt: "UW-Eau Claire Club Pickleball group picture",
     heading: "UW-Eau Claire Pickleball Club",
     year: "2024",
     text: "Colin first got into pickleball when he joined the Club Pickleball Team at the University of Wisconsin-Eau Claire. A year after joining, he became Co-President of the club where he organized practices and team tournaments.",
@@ -11,7 +11,7 @@ export const pickleballJourneyData = [
   {
     id: 2,
     src: "./tournament-pic.JPG",
-    alt: "Colin and his Mixed Doubles Pickleball Partner",
+    alt: "Colin and his mixed doubles pickleball partner",
     heading: "Local and Tournament Play",
     year: "2025",
     text: "After graduating from the University of Wisconsin-Eau Claire, Colin wanted to continue strengthening his pickleball skills and meeting new people. Currently, he practices frequently with local pickleball players in Madison. He also plays frequently in men's and mixed doubles tournaments.",
